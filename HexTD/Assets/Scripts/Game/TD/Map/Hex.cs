f5b51cbbe85fe5913c84373cd59417a7e4d3527d
Oskar <kogutoskar123@gmail.com>
2020-01-31 19:36:44 +0100

    Added hex highlight

---
+++ HexTD/Assets/Scripts/Game/TD/Map/Hex.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.TD.Map
+{
+    public class Hex : MonoBehaviour
+    {
+        private Material _material;
+        private Color _startingColor;
+
+        private void Awake()
+        {
+            _material = gameObject.GetComponent<Renderer>().material;
+            _startingColor = _material.color;
+        }
+
+        private void OnMouseOver()
+        {
+            _material.color = Color.yellow;
+        }
+
+        private void OnMouseExit()
+        {
+            _material.color = _startingColor;
+        }
+    }
+}
